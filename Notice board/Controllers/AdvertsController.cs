2eae69d0982dd2d721f17d3b84942b9a7d849993
SergeyZaporojech <91011052+SergeyZaporojech@users.noreply.github.com>
2023-03-29 22:43:17 +0300

    Add delete and ditail advert

--- Notice board/Controllers/AdvertsController.cs
+++ Notice board/Controllers/AdvertsController.cs
@@ -7,10 +7,10 @@ namespace Notice_board.Controllers
     public class AdvertsController : Controller
     {
 
-        List<Advert> adverts = new List<Advert>()
+        static List<Advert> adverts = new List<Advert>()
         {
         new Advert(){Id=1, Name="MacBook 2019", Category="Leptop", City="Rovno",Description="Normal view",ContactInformation="0974585652"},
-        new Advert(){Id=2, Name="Iphone 13", Category="Smartphone", City="Luchk",Description="Cool view",ContactInformation="06345684521"},
+        new Advert(){Id=2, Name="Iphone 13", Category="Smartphone", City="Luchk",Description="Cool view",ContactInformation="0634584521"},
         new Advert(){Id=3, Name="MacBook 2021", Category="Leptop", City="Lviv",Description="New",ContactInformation="0665241245"}
         };
         //private readonly ILogger<AdvertsController> _logger;
@@ -27,6 +27,25 @@ namespace Notice_board.Controllers
             //get adverts from database
             return View(adverts);
         }
+        public IActionResult DeleteAdvert(int id)
+        {
+             var item = adverts.FirstOrDefault(a => a.Id == id);
+            if (item == null)
+                return NotFound();
+            
+            adverts.Remove(item);            
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DitailsAdvert(int id) 
+        {
+            var item = adverts.FirstOrDefault(a=>a.Id==id);
+            if (item == null)
+                return NotFound();
+
+            return View(item);
+        }
+
     }
 
     //class AdvertViewModel
